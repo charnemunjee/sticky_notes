fcc8b2b3992ecb22484b8f914ff4886d09c9c7ff
charnemunjee <109235879+charnemunjee@users.noreply.github.com>
2026-04-14 12:52:23 +0200

    Add files via upload

--- sticky_notes/posts/tests.py
+++ sticky_notes/posts/tests.py
@@ -1,6 +1,5 @@
 import os
 from django.test import TestCase
-from django.urls import reverse
 from .models import Post
 # Set the environment variable to your project's settings module
 
@@ -8,50 +7,38 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sticky_notes.settings')
 
 
 class PostTests(TestCase):
-    def setUp(self):
+    def test_add_post(self):
         """Set up test data for Post model tests."""
         self.post = Post.objects.create(
             title='Test Post',
             content='This is a test post.'
         )
-        Post.objects.create(
+
+        self.post = Post.objects.create(
             title='Another Test Post',
-            content='This is a anothertest post.'
+            content='This is a another test post.'
         )
 
-    def test_post_has_title(self):
+    def test_post_detail(self):
         """Test that the Post model has a title
         field and the title is correctly set."""
+        self.test_add_post()  # Ensure that the test data is created
         post = Post.objects.get(id=1)
         self.assertEqual(post.title, 'Test Post')
-
-    def test_post_has_content(self):
-        """Test that the Post model has a
-        content field and the content is correctly set."""
-        post = Post.objects.get(id=1)
         self.assertEqual(post.content, 'This is a test post.')
 
-
-class PostViewTests(TestCase):
-    """Test the views for the Post model."""
-    def setUp(self):
-        self.post = Post.objects.create(
-            title='Test Post',
-            content='This is a test post.'
-        )
-
-    def test_post_list_view(self):
-        """Test that the post list view returns a 200
-        status code and contains the post title."""
-        response = self.client.get(reverse('post_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Post')
-
-    def test_post_detail_view(self):
-        """Test that the post detail view returns a 200
-        status code and contains the post title and content."""
+    def test_post_update(self):
+        """Test that we can update a Post instance."""
+        self.test_add_post()
         post = Post.objects.get(id=1)
-        response = self.client.get(reverse('post_detail', args=[str(post.id)]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Post')
-        self.assertContains(response, 'This is a test post.')
+        post.content = 'This is an updated test post.'
+        post.save()
+        self.assertEqual(post.content, 'This is an updated test post.')
+
+    def test_post_delete(self):
+        self.test_add_post()
+        """Test that we can delete a Post instance."""
+        sticky_note_count = Post.objects.count()
+        post = Post.objects.get(id=1)
+        post.delete()
+        self.assertEqual(Post.objects.count(), sticky_note_count - 1)
